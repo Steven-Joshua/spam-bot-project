1eb1b58034023f91ac47bcbb5553f01dff7f455e
Steven-Joshua <162411084+Steven-Joshua@users.noreply.github.com>
2025-03-23 14:19:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 # Store PostgreSQL database credentials in environment variables
-DB_HOST = os.getenv("DB_HOST", "your_db_host")
+DB_HOST = os.getenv("DB_HOST", "dpg-cvfsg6hopnds73bcr2gg-a")
 DB_PORT = os.getenv("DB_PORT", "5432")  # Default PostgreSQL port
-DB_USER = os.getenv("DB_USER", "your_db_user")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "your_db_password")
-DB_NAME = os.getenv("DB_NAME", "your_db_name")
+DB_USER = os.getenv("DB_USER", "spam_database_user")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "WY3goEw3okNjULxVNPJGhgbzZSWUk2ZF")
+DB_NAME = os.getenv("DB_NAME", "spam_database")
 
 # Construct the PostgreSQL connection URL
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
